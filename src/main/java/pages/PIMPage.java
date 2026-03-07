1d7589dfa3e230db6c8cadce0b39fb7bac3269ff
Duy Tester <duyptr1998@gmail.com>
2026-03-07 20:22:03 +0700

    fix

--- src/main/java/pages/PIMPage.java
+++ src/main/java/pages/PIMPage.java
@@ -80,7 +80,6 @@ public class PIMPage extends BasePage {
         // 1. Đợi ô input xuất hiện và dùng phím tắt để xóa sạch (Clear "bất tử")
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//label[text()='Employee Id']/ancestor::div[contains(@class,'oxd-input-group')]//input")));
         ui.clearInput(empIdInput);
-        ui.waitToDisplay(empIdInput);
         ui.sendKeysToElement(empIdInput, empId);
 
         // 2. Nhấn Search
